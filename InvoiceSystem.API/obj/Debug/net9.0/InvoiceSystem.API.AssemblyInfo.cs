d19449ac538c2c333db341bd989cf38b62420bce
Dushyant Tupkar <dushyant.tupkar@adp.com>
2025-07-15 13:17:17 +0530

    added angular UI

--- InvoiceSystem.API/obj/Debug/net9.0/InvoiceSystem.API.AssemblyInfo.cs
+++ InvoiceSystem.API/obj/Debug/net9.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260687fa07eef46d5d5cfb03322e7244790a20dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
